4b503d0c12a8c2662fb2d337b60a6163ea43f6e4
deoresulakshana <deoresulakshana@gmail.com>
2024-07-26 10:56:07 +0530

    Add project files.

---
+++ MoreOnDelegatesApp/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MoreOnDelegatesApp
+{
+
+    delegate void SayHello(string message);
+
+    //delegate int AddNos(int i, int j);//Func lambda
+    internal class Program
+    {
+        static int add(int i, int j) {return i + j;}
+        private static void Main(string[] args)
+        {
+
+            //SayHello h = delegate (string message) {
+            //    Console.WriteLine(message);
+
+
+            //};
+
+            //h("Good Morning!!!");
+
+            //AddNos a = (int i, int j) => { return i + j; };
+
+      
+            //Func---last parameter in list
+            Func<int, int, int> a = (i, j) => { return i + j; };
+
+            int addnos=a(1, 2);
+
+            Func<float,float,double> addFloats=( a1, b1)=> { return a1 + b1; };
+
+            double ansFloats=addFloats(1.2f, 3.4f);
+            Console.WriteLine(ansFloats);
+
+            //Action---void
+            Action<int, int> actionLambda = (int i, int j) => {
+                int c = i + j; 
+                Console.WriteLine(c);
+            };
+
+            Func<string, string, bool> validateUser = (username, password) => {
+              
+                bool status = false;
+                if (username == "Jack" && password == "Jack@123")
+                { 
+                status=true;
+                
+                }
+               return status;
+            };
+
+
+            bool validUser=validateUser("Jack", "Jack@123");
+            Console.WriteLine(validUser);
+
+
+            Func<int> f1 = () => {
+                int i = 10;
+                return i;
+            };
+
+            int output=f1();
+            Console.WriteLine(output);
+
+
+            Predicate<int> FindEven=(i)=>{
+                bool status = false;
+                if (i % 2 == 0) 
+                { 
+                    status = true; 
+                }
+                    
+                return status;
+           
+            };
+
+            bool y=FindEven(10);
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ MoreOnDelegatesApp/WorkingWithList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace MoreOnDelegatesApp
+{
+    internal class WorkingWithList
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+            list.Add(11);
+            list.Add(2);
+            list.Add(310);    
+            list.Add(41);
+            list.Add(5);
+
+            //function lambda
+            List<int> ordered = list.OrderBy(e => e).ToList();
+            ordered.ForEach(e => Console.WriteLine(e));
+
+            
+
+
+
+            //Predicate Lambda
+            // int firstOccurence=list.Find(e=>e%2!=0);
+            // Console.WriteLine(firstOccurence);
+
+
+            //Predicate Lambda
+            //List<int> AllEvenNos= list.FindAll(e => e % 2 == 0);
+
+            //Action Lambda
+            // AllEvenNos.ForEach(e=>Console.WriteLine(e));
+
+            //foreach (var item in AllEvenNos)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
